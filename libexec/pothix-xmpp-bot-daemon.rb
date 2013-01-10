81e96831679409be3541e3b4b82bca642ae6f2c9
PotHix <pothix@pothix.com>
2013-01-10 08:41:37 -0200

    Refactoring the code.

--- libexec/pothix-xmpp-bot-daemon.rb
+++ libexec/pothix-xmpp-bot-daemon.rb
@@ -1,5 +1,3 @@
-require "yaml"
-
 DaemonKit::Application.running!
 
 require 'blather/client'
@@ -7,18 +5,20 @@ require 'blather/client'
 @config = DaemonKit::Config.load('xmpp')
 setup @config.jabber_id, @config.password
 
-send_to = Blather::JID.new 'willian.molinari@gmail.com'
+send_to = Blather::JID.new @config["masters"].first
 
 subscription :request? do |s|
   write_to_stream s.approve!
 end
 
 message :chat?, :body do |m|
-  puts "resposta: #{m.body}"
+  DaemonKit.logger.info "Message: #{m.body}"
+  say send_to, m.body
 end
 
 when_ready do
-  puts "Connected #{jid}"
+  DaemonKit.logger.info "Connected #{jid}"
+
   subs = Blather::Stanza::Presence::Subscription.new send_to, :subscribe
   write_to_stream subs.request!
 
